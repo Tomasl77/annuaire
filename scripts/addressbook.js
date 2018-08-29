aafa5b98af2b38133a859b9d77e8be4504ef26a8
Tomasl77 <lobgeois.tomas@free.fr>
2018-08-29 21:39:23 +0200

    still in progress

--- scripts/addressbook.js
+++ scripts/addressbook.js
@@ -106,7 +106,6 @@ window.onload = function(){
 			showAddressBook();
 		
 		/*function editbtn(){
-
 		
 		editAll = JSON.parse(localStorage.getItem("addbook"))[editID];
 		var edition = editfullname.value!='' && editfirstname.value!='' && editphone.value!='' && editfonction.value!='' && editemail.value!='';
